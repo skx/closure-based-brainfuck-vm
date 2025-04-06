b3917aef0bd6c4178eed0c2be3cd0ab26b5be803
Steve Kemp <steve@steve.org.uk>
2025-04-06 21:53:05 +0300

    Minor reorg and comment-update

--- main.go
+++ main.go
@@ -7,25 +7,55 @@ package main
 
 import "fmt"
 
-// Type VM contains the virtual machine, it mostly exists to host a stack and a
-// set of programs.
+// VM contains the virtual machine, it mostly exists to host a stack and a
+// programs which has been "compiled" into a series of function-pointers.
+//
+// The VM will execute a program by calling each function-pointer in turn,
+// and those functions will be closures that mutate the state of the VM as
+// side-effects.
 type VM struct {
 	// stack contains arguments which are passed to our minimal operations.
-	// We support +, -, * & / operations only.
 	stack []float64
 
 	// program contains the program we're going to execute.
-	// This is hardcoded.
 	program []vmFunc
 }
-
-// vmFunc is the type-signature of a primitive we've implemented.
-type vmFunc func(vm *VM) int
 
 // New is the VM constructor
 func New(prog []vmFunc) *VM {
 	return &VM{program: prog}
 }
+
+// RunProgram executes the program which was given in the constructor.
+func (vm *VM) RunProgram() {
+
+	// Reset the state of the stack each run.
+	vm.stack = []float64{}
+
+	// Execute the program.
+	code := vm.program
+	ip := 0
+
+	// For each operation.  Run it
+	for ip < len(code) {
+
+		// Here the return value controls which operation
+		// we execute next by changing the offset within
+		// the code-array.
+		//
+		// We could allow loops by having the opcodes return
+		// different values.  In this example we only move
+		// forwards.
+		ip += code[ip](vm)
+	}
+}
+
+//
+// Okay here we write some helpers which create/return closures
+//
+
+// vmFunc is the type-signature of a mutating primitive we would implement.
+type vmFunc func(vm *VM) int
 
 // newInt creates, and returns, a closure which adds the float value to the program stack.
 func newInt(n float64) vmFunc {
@@ -35,8 +65,9 @@ func newInt(n float64) vmFunc {
 	}
 }
 
-// mathOp creates, and returns, a closure which adds a maths operation to the
-// program.
+// mathOp creates, and returns, a closure which adds a mathematical operation to the program.
+//
+// We're stack-based so we pop our arguments, run the operation, and push the result.
 func mathOp(op string) vmFunc {
 
 	return func(v *VM) int {
@@ -64,6 +95,7 @@ func mathOp(op string) vmFunc {
 	}
 }
 
+// printOp shows the value at the top of the stack.
 func printOp() vmFunc {
 	return func(v *VM) int {
 		if len(v.stack) > 0 {
@@ -75,30 +107,11 @@ func printOp() vmFunc {
 	}
 }
 
-// RunProgram creates a simple program and executes it.
-func (vm *VM) RunProgram() {
+//
+// Closure time is over now.
+//
 
-	// The stack is empty remember.
-
-	// Execute the program.
-	code := vm.program
-	ip := 0
-
-	// For each operation.  Run it
-	for ip < len(code) {
-
-		// Here the return value controls which operation
-		// we execute next by changing the offset within
-		// the code-array.
-		//
-		// We could allow loops by having the opcodes return
-		// different values.  In this example we only move
-		// forwards.
-		ip += code[ip](vm)
-	}
-}
-
-// main is our entrypoint and creates/runs a program.
+// main is our entry-point and creates/runs a program.
 func main() {
 
 	// Create a basic program.
@@ -107,6 +120,7 @@ func main() {
 	prog = append(prog, newInt(7))
 	prog = append(prog, mathOp("+")) // 3 + 7
 	prog = append(prog, printOp())
+
 	prog = append(prog, newInt(4.5)) // * 4.5
 	prog = append(prog, mathOp("*"))
 	prog = append(prog, printOp())
